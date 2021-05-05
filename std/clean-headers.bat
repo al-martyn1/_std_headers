b39f6dcdc9dce0405507154c17dfa7e1432984bf
Alexander Martynov <amart@mail.ru>
2021-05-06 02:09:52 +0300

    Added removing git-add.bat

--- std/clean-headers.bat
+++ std/clean-headers.bat
@@ -1,28 +1,35 @@
 @rem Add -d=MACROS or --define=MACROS to generate includes for macros
 
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 
 @cd chrono
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
 @cd execution
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
 @cd numbers
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
 @cd placeholders
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
 @cd pmr
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
 @cd ranges
 umba-make-headers --clean namelist.txt
+@if exist git-add.bat del git-add.bat
 @cd ..
 
