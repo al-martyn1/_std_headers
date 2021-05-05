4ee01936ccf4ce75a24f6df304a905add4f555b4
Alexander Martynov <amart@mail.ru>
2021-05-06 02:10:59 +0300

    Added calling git-add.bat

--- std/make-headers.bat
+++ std/make-headers.bat
@@ -1,28 +1,35 @@
 @rem Add -d=MACROS or --define=MACROS to generate includes for macros
 
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 
 @cd chrono
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd execution
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd numbers
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd placeholders
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd pmr
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd ranges
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@if exist git-add.bat call git-add.bat
 @cd ..
 
