49d9686188f6c7b5ba0e2f652bf446c7526963f4
Alexander Martynov <amart@mail.ru>
2021-05-06 02:17:08 +0300

    Generated std headers

---
+++ std/git-add-all.bat
@@ -0,0 +1,26 @@
+@if exist git-add.bat call git-add.bat
+
+@cd chrono
+@if exist git-add.bat call git-add.bat
+@cd ..
+
+@cd execution
+@if exist git-add.bat call git-add.bat
+@cd ..
+
+@cd numbers
+@if exist git-add.bat call git-add.bat
+@cd ..
+
+@cd placeholders
+@if exist git-add.bat call git-add.bat
+@cd ..
+
+@cd pmr
+@if exist git-add.bat call git-add.bat
+@cd ..
+
+@cd ranges
+@if exist git-add.bat call git-add.bat
+@cd ..
+
--- std/make-headers.bat
+++ std/make-headers.bat
@@ -1,35 +1,28 @@
 @rem Add -d=MACROS or --define=MACROS to generate includes for macros
 
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 
 @cd chrono
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd execution
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd numbers
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd placeholders
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd pmr
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
 @cd ranges
 umba-make-headers -W=all  namelist.txt                  > make-headers.log
-@if exist git-add.bat call git-add.bat
 @cd ..
 
