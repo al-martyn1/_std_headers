d32dc2deef92c4a4d7f749b4604315a9a308c245
Alexander Martynov <amart@mail.ru>
2021-05-11 00:18:20 +0300

    Added generated headers

--- std/make-headers.bat
+++ std/make-headers.bat
@@ -1,28 +1,30 @@
 @rem Add -d=MACROS or --define=MACROS to generate includes for macros
 
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+@set OPTS=-W=all --git-add
+
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 
 @cd chrono
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
 @cd execution
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
 @cd numbers
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
 @cd placeholders
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
 @cd pmr
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
 @cd ranges
-umba-make-headers -W=all  namelist.txt                  > make-headers.log
+umba-make-headers %OPTS%  namelist.txt                  > make-headers.log
 @cd ..
 
